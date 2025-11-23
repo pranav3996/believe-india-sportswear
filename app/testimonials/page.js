f9a2c3921f01b3af2623e33973e7deb9ce92829a
pranav3996 <54428844+pranav3996@users.noreply.github.com>
2025-11-23 22:23:28 +0530

    bug fix test

--- app/testimonials/page.js
+++ app/testimonials/page.js
@@ -98,7 +98,7 @@ export default function TestimonialsPage() {
 
                                 {/* Quote */}
                                 <p className="text-gray-700 mb-6 italic">
-                                    &ldquo;{testimonial.text || testimonial.comment}&rdquo;
+                                    {testimonial.text || testimonial.comment}
                                 </p>
 
                                 {/* Author */}
